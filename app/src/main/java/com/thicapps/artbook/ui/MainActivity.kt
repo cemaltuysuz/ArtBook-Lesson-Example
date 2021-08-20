c1196c36f1f0b5cdd15a9eac0200368d04149b44
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-20 13:31:07 +0300

    Created Recycler Adapter for Art Details Fragment

--- app/src/main/java/com/thicapps/artbook/ui/MainActivity.kt
+++ app/src/main/java/com/thicapps/artbook/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.thicapps.artbook.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentFactory
 import com.thicapps.artbook.R
+import com.thicapps.artbook.ui.fragments.ArtFragmentFactory
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var fragmentFactory: ArtFragmentFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
+
     }
 }
