24ddbb7d774662b8e0d3861e0684a4dddbc513e1
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-23 16:50:48 +0300

    safe

--- app/src/main/java/com/thicapps/artbook/ui/MainActivity.kt
+++ app/src/main/java/com/thicapps/artbook/ui/MainActivity.kt
@@ -18,6 +18,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportFragmentManager.fragmentFactory = fragmentFactory
         setContentView(R.layout.activity_main)
-
     }
 }
